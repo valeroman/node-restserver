9438fe03f4b0c0f9d43867f93efca4e1db49ec22
Roman <valeroman@gmail.com>
2020-07-03 15:31:21 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,40 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', (req, res) => {
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, (err, res) => {
 
-    let body = req.body;
+    if (err) throw err;
 
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-
-        res.json({
-            persona: body
-        });
-
-    }
-
-
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario');
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
